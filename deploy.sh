0b749f3fc0d624ea5ff0dd73dc99948398bebda9
Mathieu <87621501+MathieuRanc@users.noreply.github.com>
2024-12-07 14:33:13 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
 
-# Variables
-remote_user="cpiy9257"
-remote_host="109.234.160.108"
-local_dir="dist/"
-remote_dir="/home/cpiy9257/avariko-athletics.com"
-
-yarn build
-yarn generate
-
-# Synchronisation des dossiers avec rsync
-rsync -avz --exclude='.DS_Store' -e "ssh" "$local_dir" "$remote_user@$remote_host:$remote_dir" --delete
-
-echo "Synchronisation terminée."
